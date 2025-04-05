19b7ef1fe757119f54087cb77dd1da9af0aefd81
cle-berr <cyplb76@gmail.com>
2025-04-05 16:24:14 +0200

    working on ex01 cpp00

---
+++ CPP_Module_00/ex01/PhoneBook.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cle-berr <cle-berr@student.42lehavre.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/05 16:20:10 by cle-berr          #+#    #+#             */
+/*   Updated: 2025/04/05 16:20:13 by cle-berr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+//#include "Contact.hpp"
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+class PhoneBook
+{
+	private:
+		//Contact _Phonebook[8];
+		int		_index;
+	
+	public:
+	
+	PhoneBook();
+	~PhoneBook();
+
+	void	add_contact_in_phonebook();
+	void	get_contact_infos();
+	
+};
